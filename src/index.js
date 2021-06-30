b4446ddeada0c443cef56944ce0c3becb31dccfb
Harry Utomo <s3510376@student.rmit.edu.au>
2021-06-30 23:49:52 +1000

    restructuring project structure and deleting unused files

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
-import awsconfig from './aws-exports';
+import awsconfig from './utils/aws-exports';
 import { ApolloProvider } from 'react-apollo';
 import { Rehydrated } from 'aws-appsync-react';
 
@@ -17,22 +16,13 @@ const client = new AWSAppSyncClient({
 	},
 });
 
-const WithProvider = () => (
-	<ApolloProvider client={client}>
-		<Rehydrated>
-			<App />
-		</Rehydrated>
-	</ApolloProvider>
-);
-
 ReactDOM.render(
 	<React.StrictMode>
-		<WithProvider />
+		<ApolloProvider client={client}>
+			<Rehydrated>
+				<App />
+			</Rehydrated>
+		</ApolloProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/utils/graphql/queries.ts
+++ src/utils/graphql/queries.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
-import { gql } from '@apollo/client';
+import { gql } from 'graphql-tag';
 
 export const ListZellerCustomers = gql`
 	query ListZellerCustomers {
